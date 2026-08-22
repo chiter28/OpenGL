4e9f59416005ea4cd60889b32d1c14285c260a43
dima <ivanovdima1999@gmail.com>
2026-08-22 11:02:33 +0300

    Sync deleted files(src)

---
+++ external/stb/stb_image.cpp
@@ -0,0 +1,5 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STBI_ONLY_PNG
+#define STBI_ONLY_JPEG
--- src/Renderer/Input/Input.h
+++
@@ -1,9 +0,0 @@
-#include <utility>
-
-class Input
-{
-public:
-	static bool IsKeyPressed(int keycode);
-	static bool IsMouseButtonPressed(int button);
-	static std::pair<float, float> GetMousePos();
-};
